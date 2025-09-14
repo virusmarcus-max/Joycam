0d6d3f51716dd7a34c872d1b9585ef62969cc6fa
virusmarcus-max <virus.marcus@gmail.com>
2025-09-14 19:19:08 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='joycam-v1';
+const CACHE='joycam-v2';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])));
 });
