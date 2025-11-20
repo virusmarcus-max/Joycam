618652fcc69a2a48abb1216b7813d8a83ff8ec75
virusmarcus-max <virus.marcus@gmail.com>
2025-11-20 11:07:25 +0100

    Update service worker cache version to v5

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='joycam-v4';
+const CACHE='joycam-v5';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])));
 });
